32b5500db7c49d165a6af3b12cb72b07cc980bbd
Matías Martínez <38102678+matiasmartinmartinez@users.noreply.github.com>
2018-06-21 09:14:53 -0300

    Estructura Shiny

--- app.R
+++ app.R
@@ -1,54 +1,66 @@
 
 
-
-
-
 library(shiny)
 
 ui <- fluidPage(
   
-  titlePanel( h1("Siniestros fatales de tránsito en Uruguay, 2013-2017"),
+  titlePanel( h1("Siniestros fatales de tránsito en Uruguay, 2013-2017")),
   
-           tabsetPanel(
-             tabPanel(
-               title = h6("Introducción"),
-               hr(),
-               p("El presente estudio surge de la curiosidad por conocer el comportamiento de los siniestros de tránsitos en Uruguay y posee como objetivo madre una profunda aplicación de los conocimientos adquiridos en la asignatura Nuevas técnicas para el análisis de datos, marcando énfasis en un correcto análisis exploratorio y la generación de adecuadas visualizaciones de los datos.Se analizara tanto los datos del fallecido como lo del suceso.")),
-             h3("Antecedentes:"),
-             p(" En su programa de Sistema de Información Nacional de Tránsito (SINATRÁN), donde se extrajo los datos, la Unidad Nacional de Seguridad Vial (UNASEV), genera un informe anual sobre la Siniestralidad Vial en Uruguay para el periodo dado desde 2009 a la actualidad. Dicho informes se pueden encontrar a través del siguiente",  a(href="http://unasev.gub.uy/inicio/sinatran/informes_siniestralidad_vial_uruguay/","enlace.")),
-             h3("Justificación:"),
-             p("Conocer la realidad uruguaya a nivel de tránsito permite no sólo generar conocimiento y concientizar acerca de la importancia en los cuidados y medidas de seguridad apropiadas en el tránsito, sino también tomar decisiones capaces de transformar un posible siniestro fatal en un accidente menor, o en mejor instancia en una posible inexistencia del mismo.
-Adicionalmente, motivar la divulgación de distintos estudios estadísticos que a día de hoy provienen únicamente de entidades gubernamentales, los mismos no solo de ámbitos de seguridad vial sino sociales en general."),
-             h3("Preguntas a responder"),
-             p("1 - ¿Cuál es la distribución relativa por sexo y qué comportamiento posee la edad del fallecido en el período dado?"),
-             p("2 - ¿Existe relación entre el rol del fallecido tanto con la edad, como con el vehículo del difunto?"),
-             p("3 -  Para observaciones de vehículo igual a peatón: ¿qué vehículo estubo involucrado en el fallecimientos de los mismos y cuál posee mayor frecuencia acumulada?"),
-             p("4 - ¿Hay relación entre el vehículo del fallecido y otro posible vehículo involucrado en el accidente?"),
-             p("5 - ¿Qué comportamiento posee la jurisdicción por si misma, y según el tipo de siniestro ocurrido?"),
-             p("6 - ¿Cuál es la distribución de los días de supervivencia luego del accidente y qué proporción ocupa cada tipo de siniestro en caso de muerte súbita?"),
-             p("7 - ¿Cuál es la tasa de fallecidos por departamento?"),
-             p("8 - ¿Qué fechas y horarios poseen mayor frecuencia absoluta de difuntos?")
+  tabsetPanel(
+    
+    
+    tabPanel(
+      title = h6("Introducción"),
+      
+        
+        hr(),
+        p("El presente estudio surge de la curiosidad por conocer el comportamiento de los siniestros de tránsitos en Uruguay y posee como objetivo madre una profunda aplicación de los conocimientos adquiridos en la asignatura Nuevas técnicas para el análisis de datos, marcando énfasis en un correcto análisis exploratorio y la generación de adecuadas visualizaciones de los datos.Se analizara tanto los datos del fallecido como lo del suceso."),
+       
+         h3("Antecedentes:"),
+        p(" En su programa de Sistema de Información Nacional de Tránsito (SINATRÁN), donde se extrajo los datos, la Unidad Nacional de Seguridad Vial (UNASEV), genera un informe anual sobre la Siniestralidad Vial en Uruguay para el periodo dado desde 2009 a la actualidad. Dicho informes se pueden encontrar a través del siguiente",  a(href="http://unasev.gub.uy/inicio/sinatran/informes_siniestralidad_vial_uruguay/","enlace."),
+       
+           h3("Justificación:"),
+        p("Conocer la realidad uruguaya a nivel de tránsito permite no sólo generar conocimiento y concientizar acerca de la importancia en los cuidados y medidas de seguridad apropiadas en el tránsito, sino también tomar decisiones capaces de transformar un posible siniestro fatal en un accidente menor, o en mejor instancia en una posible inexistencia del mismo. Adicionalmente, motivar la divulgación de distintos estudios estadísticos que a día de hoy provienen únicamente de entidades gubernamentales, los mismos no solo de ámbitos de seguridad vial sino sociales en general."),
+       
+         h3("Preguntas a responder"),
+        p("1 - ¿Cuál es la distribución relativa por sexo y qué comportamiento posee la edad del fallecido en el período dado?"),
+        p("2 - ¿Existe relación entre el rol del fallecido tanto con la edad, como con el vehículo del difunto?"),
+        p("3 -  Para observaciones de vehículo igual a peatón: ¿qué vehículo estubo involucrado en el fallecimientos de los mismos y cuál posee mayor frecuencia acumulada?"),
+        p("4 - ¿Hay relación entre el vehículo del fallecido y otro posible vehículo involucrado en el accidente?"),
+        p("5 - ¿Qué comportamiento posee la jurisdicción por si misma, y según el tipo de siniestro ocurrido?"),
+        p("6 - ¿Cuál es la distribución de los días de supervivencia luego del accidente y qué proporción ocupa cada tipo de siniestro en caso de muerte súbita?"),
+        p("7 - ¿Cuál es la tasa de fallecidos por departamento?"),
+        p("8 - ¿Qué fechas y horarios poseen mayor frecuencia absoluta de difuntos?")
              
-             ),
-             
-             
-             
-             tabPanel(
+             )),                       
+  
+            tabPanel(
                title = h6("Marco Teórico"),
+               hr(),
                h3("Fuente de datos"),
-               
-               
+               p(" Los datos fueron obtenidos de la Unidad Nacional de Seguridad Víal (UNASEV), correspondientes al período comprendido entre 2013 y 2017. Originalmennte los datos correspondían al año 2017, sin embargo se identificó apropiada la utilización de una serie de tiempo por lo cual se mecharon bases desde el año 2013, esto provocó ciertos inconvenientes en cuanto a nombres de variables debido a la utilizacion de tildes o el aumento de variables para ciertos años pero finalmente fueron solucionados y concluimos en una base de datos apta y abarcativa."),
+               p("Mediante su catálogo de datos abiertos se extraen las bases utilizadas, desde el siguiente",a(href="http://unasev.gub.uy/inicio/sinatran/datos_abiertos/","url"),
+                
+                  h3("Descripción de variables:"),
+                 p("Se poseen 15 variables identificadas de la siguiente manera:")
+                 
+                )),
+             
+             tabPanel(title = h6("Análisis de datos"),
+                      hr(),
+                      p(" Inicialmente se llevó a cabo una profunda exploración de datos, la cual permitió además de conocer inconvenienes en la base, formular nuevas preguntas. Posterior a esto, se realizaron visualizaciones acordes con el fin de generar una respuesta clara y acorde a las dudas planeadas. A continuación se planean las mismas:")
                       ),
              
-             tabPanel(title = h6("Análisis de datos")                          ),
+             tabPanel(title = h6("Conclusiones")                       
+                      ),
              
-             tabPanel(title = h6("Conclusiones")                                 ),
-             
-             tabPanel(title = h6("Interpretaciones")             ),
-             
-             tabPanel(title = h6("Anexo")             )
+             tabPanel(title = h6("Interpretaciones")       
+                      ),
+  
+          tabPanel(title = h6("Anexo")  
+                      )
            )
-    )
+  )
   
 
 
