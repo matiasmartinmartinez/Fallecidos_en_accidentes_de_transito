beb12cd1cfaf68d8a4f085623844c3c6f3404e04
Matías Martínez <38102678+matiasmartinmartinez@users.noreply.github.com>
2018-06-23 14:28:34 -0300

    .

--- Aplicación Shiny/app.R
+++ Aplicación Shiny/app.R
@@ -1,4 +1,3 @@
-
 ################################################################################################
 #A continuación se cargan paquetes y datos a utilizar, además se genera una paleta de colores elegida arbitariamente.
 
@@ -14,7 +13,7 @@ paquetes.a.utilizar<- c( "tidyverse", "rmarkdown", "shiny", "ggmosaic", "plotly"
 ipack(paquetes.a.utilizar)
 
 #Datos a utilizar
-load("base_datos___fallecidos_transito_uruguay_2013-2017.RData")
+#load("base_datos___fallecidos_transito_uruguay_2013-2017.RData")
 #Paleta
 colores<-c("darkolivegreen3","turquoise4","tan2","indianred",
            "khaki3", "thistle4", "lightsteelblue","grey80")
@@ -71,38 +70,23 @@ censo<- c(73378,520187,84698,123203,57088, 25050,67048,58815,164300,1319108,   1
 # Tasa de fallecidos en accidentes de tránsito cada 10.000 habitantes.
 pob.dep<- cbind((datos %>% count(dep) %>% arrange(dep)),censo)%>% mutate(tasa=(n/censo)*10000)
 
-
-
-
 #Cargar fechas
-fecha<-    datos %>% separate(  fecha,c("dia","mes")  ) %>% 
-  mutate( f.h=  paste( paste(a,mes,dia,sep="-") ,  hora )  ) %>%
-  mutate( f.h=  as.POSIXct(f.h, format="%Y-%m-%d  %H:%M:%S",tz="GMT"))%>%
+fecha<- datos %>% separate( fecha,c("dia","mes") ) %>%
+  mutate( f.h= paste( paste(a,mes,dia,sep="-") , hora ) ) %>%
+  mutate( f.h= as.POSIXct(f.h, format="%Y-%m-%d %H:%M:%S",tz="GMT"))%>%
   dplyr::select(f.h)
 
-
-n.dia <-  weekdays(       as.Date(  as.vector(t (fecha[1])  )    ) )
+n.dia <- weekdays( as.Date( as.vector(t (fecha[1]) ) ) )
 
 f<- data.frame(fecha,n.dia)
 
-
-
 ################################################################################################
-
-
-
-
-
-
-
-
 
 ui <- fluidPage(
   
   titlePanel( h1("Siniestros fatales de tránsito en Uruguay, 2013-2017")),
   
   tabsetPanel(
-    
     
     tabPanel(
       title = h6("Distribución territorial"),
@@ -115,15 +99,15 @@ ui <- fluidPage(
           checkboxGroupInput("años", "Elegir Año:",
                              choiceNames = c("2013", "2014", "2015", "2016","2017"),
                              choiceValues =  c("2013", "2014", "2015", "2016","2017"))
-          ),
+        ),
         
         
         mainPanel(
           plotOutput("mapa")
-         
-          )
+          
         )
-      ),  
+      )
+    ),  
     
     
     
@@ -149,9 +133,9 @@ ui <- fluidPage(
         mainPanel(   
           plotlyOutput("densidad"),
           p(" facetear por sexo, indicar vehiculo y año")
-          )
+        )
       )
-      ),
+    ),
     
     
     tabPanel(
@@ -165,27 +149,27 @@ ui <- fluidPage(
           selectInput("variablemosaico", "Variable:",
                       choices = c("Sexo","Rol", "Jurisdicción")),
           
-       conditionalPanel(
-         condition = "input.variablemosaico!='Sexo'",
-         selectInput("sexomosaico", "Sexo:",
-                                    choices = c("Facetear", "No facetear") ))          ,
+          conditionalPanel(
+            condition = "input.variablemosaico!='Sexo'",
+            selectInput("sexomosaico", "Sexo:",
+                        choices = c("Facetear", "No facetear") ))          ,
           
           checkboxGroupInput("añosmosaico", "Elegir Año:",
                              choiceNames = c("2013", "2014", "2015", "2016","2017"),
                              choiceValues =  c("2013", "2014", "2015", "2016","2017"))
           
           
-      
+          
           
         ),
         mainPanel( 
-      
-      plotlyOutput("rol"),
-      plotlyOutput("involucrado"),
-      p("sexo y año para ambos,   rol y jurisdiccion  para involucrado ")
-         )
+          
+          plotlyOutput("rol"),
+          plotlyOutput("involucrado"),
+          p("sexo y año para ambos,   rol y jurisdiccion  para involucrado ")
+        )
       )
-      ),
+    ),
     
     tabPanel(
       title = h6("Series de tiempo"),
@@ -202,23 +186,18 @@ ui <- fluidPage(
           checkboxInput("puntoylinea", "Punto y línea", TRUE)
         ),
         mainPanel(
-      plotOutput("fecha")
-    
-      )
+          plotOutput("fecha")
+          
+        )
       )
     )))
 
 ################################################################################################
 
-
-
-
-
-server <-  function(input, output) {
-
+server <- function(input, output) {
   
   output$mapa <- renderPlot({
-  
+    
     mapeo(n= (datos %>% filter( a==input$años |
                                   a==input$años |
                                   a==input$años |
@@ -235,70 +214,67 @@ server <-  function(input, output) {
         mid = "white",
         high = "#5ab4ac",
         midpoint =  mean((datos %>% filter( a==input$años |
-                                              a==input$años |
-                                              a==input$años |
-                                              a==input$años |
-                                              a==input$años |
-                                              a==input$años ) %>% count(a,dep) %>% arrange(a) %>% mutate(
+                                  a==input$años |
+                                  a==input$años |
+                                  a==input$años |
+                                  a==input$años |
+                                  a==input$años  ) %>% count(a,dep) %>% arrange(a) %>% mutate(
           censo= (rep(censo,(length(table(a)))))) %>% mutate(n=(n/censo)*10000))$n )) +
       facet_wrap( ~ a)
     
   })
   
-  
-  output$densidad <- renderPlotly({ 
+  output$densidad <- renderPlotly({
     ggplotly(
-    
-    datos %>% filter(vehi== "AUTO" | vehi=="MOTO")%>% 
-    ggplot(aes(edad)) +
-    geom_density(
-      aes(fill = vehi), 
-      alpha = 0.5, 
-      show.legend = T) +
-    labs(
-      x =  "Edad", 
-      y = "Densidad")  + 
-    theme_minimal() + 
-    guides(fill = guide_legend(title = "Vehículo:")) +
-    theme(
-      legend.position = "bottom", 
-      axis.title  = (element_text(
-        size = 12,
-        colour = "darkslategray"))) + 
-    scale_fill_manual (values=colores))
-    })
+      
+      datos %>% filter(vehi== "AUTO" | vehi=="MOTO")%>% 
+        ggplot(aes(edad)) +
+        geom_density(
+          aes(fill = vehi), 
+          alpha = 0.5, 
+          show.legend = T) +
+        labs(
+          x =  "Edad", 
+          y = "Densidad")  + 
+        theme_minimal() + 
+        guides(fill = guide_legend(title = "Vehículo:")) +
+        theme(
+          legend.position = "bottom", 
+          axis.title  = (element_text(
+            size = 12,
+            colour = "darkslategray"))) + 
+        scale_fill_manual (values=colores))
+  })
   
-  output$rol <- renderPlotly({ 
-  ggplotly(
-    datos  %>%  filter(a=="2013")%>% filter(sexo=="F")%>%dplyr::count(vehi, rol) %>% filter(vehi!="PEATON")  %>%
-      group_by( vehi= ifelse ( 
-        (vehi!="BICICLETA" &
-           vehi!="MOTO" & 
-           vehi!="AUTO" & 
-           vehi!="BICICLETA" & 
-           vehi!="CAMIONETA"),"OTROS", vehi ),rol)  %>%
-      summarise(n=sum(n))  %>%
-      ggplot()  +
-      geom_mosaic(aes(
-        weight = n,
-        x = product(  reorder(abbreviate(vehi, 4),-n)  ),
-        fill = abbreviate( rol, 1) ),
-        alpha=0.64)  +
-      labs(
-        x = "Vehículo del fallecido",
-        y = "Proporción de fallecidos") +
-      guides(fill = guide_legend(title = "Rol")) +
-      theme_minimal() +
-      theme(
-        axis.title =
-          element_text(
-            size = 12,colour="grey30")) +
-      scale_fill_manual(  values = colores[c(2,3,1)]))
-    })
+  output$rol <- renderPlotly({
+    ggplotly(
+      datos %>% filter(a=="2013")%>% filter(sexo=="F")%>%dplyr::count(vehi, rol) %>% filter(vehi!="PEATON") %>%
+        group_by( vehi= ifelse (
+          (vehi!="BICICLETA" &
+             vehi!="MOTO" &
+             vehi!="AUTO" &
+             vehi!="BICICLETA" &
+             vehi!="CAMIONETA"),"OTROS", vehi ),rol) %>%
+        summarise(n=sum(n)) %>%
+        ggplot() +
+        geom_mosaic(aes(
+          weight = n,
+          x = product( reorder(abbreviate(vehi, 4),-n) ),
+          fill = abbreviate( rol, 1) ),
+          alpha=0.64) +
+        labs(
+          x = "Vehículo del fallecido",
+          y = "Proporción de fallecidos") +
+        guides(fill = guide_legend(title = "Rol")) +
+        theme_minimal() +
+        theme(
+          axis.title =
+            element_text(
+              size = 12,colour="grey30")) +
+        scale_fill_manual( values = colores[c(2,3,1)]))
+  })
   
-  
-  
-  output$involucrado <- renderPlotly({ 
+  output$involucrado <- renderPlotly({
     ggplotly(
       datos %>% filter(a=="2017") %>%dplyr::count(involucrado, sexo) %>%
         ggplot() +
@@ -320,62 +296,58 @@ server <-  function(input, output) {
             vjust = -2),
           axis.title = element_text(
             size=12,
-            colour="grey20")) + 
+            colour="grey20")) +
         scale_fill_manual(
           values = colores) +
         guides(
           fill= guide_legend("Involucrado")))
-
+    
   })
-  
-
-  
   
   #SERIES DE TIEMPO
   
-    intervaloInput <- reactive({
+  intervaloInput <- reactive({
     switch(input$intervalo,
            "1 mes"= "1 month",
            "3 mes"= "3 months",
            "6 meses" = "6 months",
            "1 año" = "1 year")
+    
   })
-
- 
   
   output$fecha <- renderPlot({
     
     if(input$fallecidos=="Total")
     {
       if(input$puntoylinea==T) {
-      
-    data.frame(datos,f) %>%
-    count( a.mes=    floor_date(f.h, intervaloInput()))  %>%
-    ggplot( 
-      aes(
-        as_date(a.mes),
-          n
-        ) ) +geom_point()+   
-        geom_line(
-          stat="identity")+
-    geom_smooth(
-      method = "loess",
-      se=input$error,
-      cex=1.4)+
-    labs(
-      x = "Año",
-      y = "Frecuencia absoluta de fallecidos") +
-    theme_minimal() +
-    theme(
-      axis.title = element_text(
-        colour="grey30", 
-        size=12),
-      axis.text =element_text(
-        colour = "grey27",
-        size=9)       )  +
-    scale_x_date( 
-      date_labels =("%Y"),
-      date_breaks = "1 year" )+
+        
+        data.frame(datos,f) %>%
+          count( a.mes=    floor_date(f.h, intervaloInput()))  %>%
+          ggplot( 
+            aes(
+              as_date(a.mes),
+              n
+            ) ) +geom_point()+   
+          geom_line(
+            stat="identity")+
+          geom_smooth(
+            method = "loess",
+            se=input$error,
+            cex=1.4)+
+          labs(
+            x = "Año",
+            y = "Frecuencia absoluta de fallecidos") +
+          theme_minimal() +
+          theme(
+            axis.title = element_text(
+              colour="grey30", 
+              size=12),
+            axis.text =element_text(
+              colour = "grey27",
+              size=9)       )  +
+          scale_x_date( 
+            date_labels =("%Y"),
+            date_breaks = "1 year" )+
           scale_color_manual(values=colores)}
       
       else {  data.frame(datos,f) %>%
@@ -404,43 +376,43 @@ server <-  function(input, output) {
             date_labels =("%Y"),
             date_breaks = "1 year" )+
           scale_color_manual(values=colores)}
-   
-  
-      } 
+      
+      
+    } 
     
     else if(input$fallecidos=="Sexo"){
       
       if(input$puntoylinea==T) {
         
-      data.frame(datos,f) %>%
-        count(sexo, a.mes=    floor_date(f.h, intervaloInput()))  %>%
-        ggplot( 
-          aes(
-            as_date(a.mes),
-            n,
-            colour=sexo
-          ) ) + 
-        geom_point()  + 
-        geom_line(
-          stat="identity")+ 
-        geom_smooth(
-          method = "loess",
-          se=input$error,
-          cex=1.4)+
-        labs(
-          x = "Año",
-          y = "Frecuencia absoluta de fallecidos") +
-        theme_minimal() +
-        theme(
-          axis.title = element_text(
-            colour="grey30", 
-            size=12),
-          axis.text =element_text(
-            colour = "grey27",
-            size=9)       )  +
-        scale_x_date( 
-          date_labels =("%Y"),
-          date_breaks = "1 year" ) }
+        data.frame(datos,f) %>%
+          count(sexo, a.mes=    floor_date(f.h, intervaloInput()))  %>%
+          ggplot( 
+            aes(
+              as_date(a.mes),
+              n,
+              colour=sexo
+            ) ) + 
+          geom_point()  + 
+          geom_line(
+            stat="identity")+ 
+          geom_smooth(
+            method = "loess",
+            se=input$error,
+            cex=1.4)+
+          labs(
+            x = "Año",
+            y = "Frecuencia absoluta de fallecidos") +
+          theme_minimal() +
+          theme(
+            axis.title = element_text(
+              colour="grey30", 
+              size=12),
+            axis.text =element_text(
+              colour = "grey27",
+              size=9)       )  +
+          scale_x_date( 
+            date_labels =("%Y"),
+            date_breaks = "1 year" ) }
       
       else {data.frame(datos,f) %>%
           count(sexo, a.mes=    floor_date(f.h, intervaloInput()))  %>%
@@ -468,42 +440,42 @@ server <-  function(input, output) {
           scale_x_date( 
             date_labels =("%Y"),
             date_breaks = "1 year" )}
-        
+      
       
     }
     else if(input$fallecidos=="Rol"){
       
       if(input$puntoylinea==T) {
         
-      data.frame(datos,f) %>%
-        count(rol, a.mes=    floor_date(f.h, intervaloInput()))  %>%
-        ggplot( 
-          aes(
-            as_date(a.mes),
-            n,
-            colour=rol
-          ) ) + 
-        geom_point()  + 
-        geom_line(
-          stat="identity")+ 
-        geom_smooth(
-          method = "loess",
-          se=input$error,
-          cex=1.4)+
-        labs(
-          x = "Año",
-          y = "Frecuencia absoluta de fallecidos") +
-        theme_minimal() +
-        theme(
-          axis.title = element_text(
-            colour="grey30", 
-            size=12),
-          axis.text =element_text(
-            colour = "grey27",
-            size=9)       )  +
-        scale_x_date( 
-          date_labels =("%Y"),
-          date_breaks = "1 year" ) +
+        data.frame(datos,f) %>%
+          count(rol, a.mes=    floor_date(f.h, intervaloInput()))  %>%
+          ggplot( 
+            aes(
+              as_date(a.mes),
+              n,
+              colour=rol
+            ) ) + 
+          geom_point()  + 
+          geom_line(
+            stat="identity")+ 
+          geom_smooth(
+            method = "loess",
+            se=input$error,
+            cex=1.4)+
+          labs(
+            x = "Año",
+            y = "Frecuencia absoluta de fallecidos") +
+          theme_minimal() +
+          theme(
+            axis.title = element_text(
+              colour="grey30", 
+              size=12),
+            axis.text =element_text(
+              colour = "grey27",
+              size=9)       )  +
+          scale_x_date( 
+            date_labels =("%Y"),
+            date_breaks = "1 year" ) +
           scale_color_manual(values=colores)}
       
       else {   data.frame(datos,f) %>%
@@ -539,35 +511,35 @@ server <-  function(input, output) {
       
       if(input$puntoylinea==T) {
         
-      data.frame(datos,f) %>%
-        count(jur, a.mes=    floor_date(f.h, intervaloInput()))  %>%
-        ggplot( 
-          aes(
-            as_date(a.mes),
-            n,
-            colour=jur
-          ) ) + 
-        geom_point()  + 
-        geom_line(
-          stat="identity")+ 
-        geom_smooth(
-          method = "loess",
-          se=input$error,
-          cex=1.4)+
-        labs(
-          x = "Año",
-          y = "Frecuencia absoluta de fallecidos") +
-        theme_minimal() +
-        theme(
-          axis.title = element_text(
-            colour="grey30", 
-            size=12),
-          axis.text =element_text(
-            colour = "grey27",
-            size=9)       )  +
-        scale_x_date( 
-          date_labels =("%Y"),
-          date_breaks = "1 year" )+
+        data.frame(datos,f) %>%
+          count(jur, a.mes=    floor_date(f.h, intervaloInput()))  %>%
+          ggplot( 
+            aes(
+              as_date(a.mes),
+              n,
+              colour=jur
+            ) ) + 
+          geom_point()  + 
+          geom_line(
+            stat="identity")+ 
+          geom_smooth(
+            method = "loess",
+            se=input$error,
+            cex=1.4)+
+          labs(
+            x = "Año",
+            y = "Frecuencia absoluta de fallecidos") +
+          theme_minimal() +
+          theme(
+            axis.title = element_text(
+              colour="grey30", 
+              size=12),
+            axis.text =element_text(
+              colour = "grey27",
+              size=9)       )  +
+          scale_x_date( 
+            date_labels =("%Y"),
+            date_breaks = "1 year" )+
           scale_color_manual(values=colores)}
       else {  
         data.frame(datos,f) %>%
@@ -600,19 +572,12 @@ server <-  function(input, output) {
       
     }
     
-    
-    
   })
   
-  
-  
-  }
-
-
+}
 
 ################################################################################################
 
 shinyApp(ui = ui, server = server)
 
 ################################################################################################
-
